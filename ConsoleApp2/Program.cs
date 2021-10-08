14ad516fcef8be6ea94b7551aff8a0566ffce4b7
Bl00mstar <mjakstefan02@gmail.com>
2021-10-08 13:01:03 -0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+//Namespace
+namespace NumberGueser
+{
+    //Main Class
+    class Program
+    {
+        //entry point method
+        static void Main(string[] args)
+        {
+            string name = "Test";
+            int age = 123;
+            Console.WriteLine("Hello " +name);
+            Console.WriteLine("{0} is {1}", name, age);
+        }
+    }
+}
